32d9266da14321bc4e1741af141bb4f9ce1f1ce1
vizucode <havisikkubaru@gmail.com>
2022-10-18 21:58:15 +0700

    rdd

--- domains/manifest/service/manifest-service.go
+++ domains/manifest/service/manifest-service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	manifestentity "manifesto/domains/manifest/entities"
 	"manifesto/exceptions"
 	"manifesto/utils/helpers"
@@ -19,17 +20,27 @@ func New(repo manifestentity.IRepoManifest) *manifestService {
 func (s *manifestService) CustomURLCreate(ManifestEntity manifestentity.Manifest) (manifestentity.Manifest, error) {
 	ManifestEntity.CustomURL = helpers.URIFormat(ManifestEntity.CustomURL)
 
-	result, _ := s.Repo.Get(ManifestEntity)
+	result, err := s.Repo.Get(ManifestEntity)
+	if err != nil {
+		fmt.Println("get first", err)
+	}
 	if result.CustomURL != "" {
 		return manifestentity.Manifest{}, exceptions.NewBadRequestError("custom url has been used")
 	}
 
-	err := s.Repo.Save(ManifestEntity)
+	err = s.Repo.Save(ManifestEntity)
 	if err != nil {
+		fmt.Println("error after save", err)
 		return manifestentity.Manifest{}, err
 	}
 
-	return s.Repo.Get(ManifestEntity)
+	result1, err := s.Repo.Get(ManifestEntity)
+	if err != nil {
+		fmt.Println(result1)
+		fmt.Println("error get second", err)
+	}
+
+	return result1, err
 }
 
 func (s *manifestService) Get(ManifestEntity manifestentity.Manifest) (manifestentity.Manifest, error) {

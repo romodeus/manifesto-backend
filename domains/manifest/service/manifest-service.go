a80f633e868574d03a6322095a754a42ed4a6c5e
vizucode <havisikkubaru@gmail.com>
2022-10-18 22:18:32 +0700

    HOTFIX

--- domains/manifest/service/manifest-service.go
+++ domains/manifest/service/manifest-service.go
@@ -19,7 +19,6 @@ func New(repo manifestentity.IRepoManifest) *manifestService {
 
 func (s *manifestService) CustomURLCreate(ManifestEntity manifestentity.Manifest) (manifestentity.Manifest, error) {
 	ManifestEntity.CustomURL = helpers.URIFormat(ManifestEntity.CustomURL)
-	fmt.Println(ManifestEntity.CustomURL)
 
 	result, _ := s.Repo.Get(ManifestEntity)
 	if result.CustomURL != "" {
@@ -32,13 +31,7 @@ func (s *manifestService) CustomURLCreate(ManifestEntity manifestentity.Manifest
 		return manifestentity.Manifest{}, err
 	}
 
-	result1, err := s.Repo.Get(ManifestEntity)
-	if err != nil {
-		fmt.Println(result1)
-		fmt.Println("error get second", err)
-	}
-
-	return result1, err
+	return s.Repo.Get(ManifestEntity)
 }
 
 func (s *manifestService) Get(ManifestEntity manifestentity.Manifest) (manifestentity.Manifest, error) {
